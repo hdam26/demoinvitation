7ee6e924d0205e6356723d750dc9b246374f9d42
Aldyaka Mushofan <Aldyaka Mushofan>
2017-10-08 06:39:02 +0700

    test map api responsiveness

--- js/main.js
+++ js/main.js
@@ -51,27 +51,6 @@ var showElement = function(scrolled, elementId, screenHeight) {
 	});
 };
 
-function initMap() {
-	var place = {
-			lat: -6.893686,
-			lng: 107.6090294
-		};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: place
-	});
-	var marker = new google.maps.Marker({
-		position: place,
-		map: map
-	});
-	
-	var detailLocation = '<div><p style="color: black; font-weight: bold;">Masjid Salman ITB</p>' + 
-						 '<p style="color: black;">Place we will get married</p></div>';
-	
-	var infoWindow = new google.maps.InfoWindow({content: detailLocation});
-	infoWindow.open(map, marker);
-}
-
 function changeStoryView() {
 	console.log('change View');
 	if (screen.width <= 575) {
@@ -98,7 +77,5 @@ $(document).ready(function() {
 	var scrollStart = 0;
 	navbarBackground(scrollStart);
 	createCountdown("November 10, 2017");
-	initMap();
 	changeStoryView();
-	window.onresize = changeStoryView();
 });
